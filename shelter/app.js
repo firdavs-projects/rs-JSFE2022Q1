9c3ba50f1c3a066549ef6956df5d9897453a94ca
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-04-10 22:36:03 +0500

    feat: add main page

---
+++ shelter/app.js
@@ -0,0 +1,60 @@
+const petInfo = [
+    {
+        name: 'Jennifer',
+        img: 'assets/images/modal-jen.png',
+        type: 'Dog - Labrador',
+        info: 'Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
+        attributes: [
+            ['Age', '2 months'],
+            ['Inoculations', 'none'],
+            ['Diseases', 'none'],
+            ['Parasites', 'none'],
+        ]
+    }
+]
+const slideButtons = document.querySelectorAll('.slide-btn');
+const modal = document.querySelector('#modal');
+const modalImg = modal.querySelector('#modal__img');
+const modalTitle = modal.querySelector('#modal__title');
+const modalType = modal.querySelector('#modal__type');
+const modalInfo = modal.querySelector('#modal__info');
+const modalAttributes = modal.querySelector('#modal__attributes');
+
+const closeModal = () => {
+    modal.classList.remove('active');
+    modalImg.src = '';
+    modalImg.alt = '';
+    modalTitle.innerHTML = '';
+    modalType.innerHTML = '';
+    modalInfo.innerHTML = '';
+    modalAttributes.innerHTML = '';
+}
+
+modal.addEventListener('click', closeModal);
+
+slideButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const current = petInfo.find(pet => pet.name === button.dataset.slide);
+        current.attributes.forEach(attribute => {
+            const dlEl = document.createElement('dl');
+            const dtEl = document.createElement('dt');
+            const ddEl = document.createElement('dd');
+            dlEl.classList.add('modal__attribute');
+            dtEl.classList.add('modal__attribute-title');
+            ddEl.classList.add('modal__attribute-value');
+            dtEl.innerHTML = attribute[0];
+            ddEl.innerHTML = attribute[1];
+            dlEl.appendChild(dtEl);
+            dlEl.appendChild(ddEl);
+            modalAttributes.appendChild(dlEl);
+        });
+        modal.classList.add('active');
+        modalImg.src = current.img
+        modalImg.alt = current.name
+        modalTitle.innerHTML = current.name;
+        modalType.innerHTML = current.type;
+        modalInfo.innerHTML = current.info;
+    })
+});
+
+
--- shelter/script.js
+++ shelter/script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const navbarLinks = document.querySelectorAll('.navbar__link');
+
+navbarLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbarLinks.forEach(link => link.classList.remove('active'));
+    link.classList.add('active');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = new ChiefSlider('.slider', {
+    autoplay: true,
+    interval: 3000,
+    swipe: true,
+    loop: true,
+  });
+});
