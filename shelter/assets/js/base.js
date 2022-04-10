7fcc8d0d10550e7995a7e60e4ed3b72511e68cb2
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-04-11 00:04:05 +0500

    feat: add pets page

--- shelter/assets/js/base.js
+++ shelter/assets/js/base.js
@@ -1,7 +1,20 @@
+// Navbar
+
+const navbarLinks = document.querySelectorAll('.navbar__link');
+
+navbarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navbarLinks.forEach(link => link.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
+// Modal
+
 const petInfo = [
     {
         name: 'Jennifer',
-        img: 'assets/images/modal-jen.png',
+        img: './assets/images/modal-jen.png',
         type: 'Dog - Labrador',
         info: 'Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won\'t hesitate to play up a storm in the house if she has all of her favorite toys.',
         attributes: [
@@ -34,7 +47,8 @@ modal.addEventListener('click', closeModal);
 
 slideButtons.forEach(button => {
     button.addEventListener('click', () => {
-        const current = petInfo.find(pet => pet.name === button.dataset.slide);
+        const current = petInfo.find(pet => pet.name === button.dataset.slide) || petInfo[0];
+        const isPetsPage = modal.dataset.page === 'pets';
         current.attributes.forEach(attribute => {
             const dlEl = document.createElement('dl');
             const dtEl = document.createElement('dt');
@@ -49,7 +63,7 @@ slideButtons.forEach(button => {
             modalAttributes.appendChild(dlEl);
         });
         modal.classList.add('active');
-        modalImg.src = current.img
+        modalImg.src = isPetsPage ? '.' + current.img : current.img;
         modalImg.alt = current.name
         modalTitle.innerHTML = current.name;
         modalType.innerHTML = current.type;
--- shelter/script.js
+++ shelter/script.js
@@ -1,12 +1,3 @@
-const navbarLinks = document.querySelectorAll('.navbar__link');
-
-navbarLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    navbarLinks.forEach(link => link.classList.remove('active'));
-    link.classList.add('active');
-  });
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   const slider = new ChiefSlider('.slider', {
     autoplay: true,
